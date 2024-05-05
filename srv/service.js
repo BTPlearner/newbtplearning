6edb01f6e4e6258410e688086dd79e31937bbda2
Nilesh Jain <nilesh.jain@piind.com>
2024-05-05 10:19:27 +0000

    init

--- srv/service.js
+++ srv/service.js
@@ -2,9 +2,16 @@ const cds = require('@sap/cds');
 module.exports = cds.service.impl(async function(srv){
     srv.on('printhelloworld',req=>{
         console.log(req.data.input)
-        return '${req.data.input} world'
+        return '$(req.data.input) world'
     }
-
+    )
+    srv.on('addition', req=>{
+        console.log(req.data)
+        let result
+        result = req.data.num1 + req.data.num2
+        return result
+    }
+    
     )
 }
 
